6030e46c8bcb84c7adeef0ca223c5413b88645c4
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-11-18 14:11:15 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get '/top' => 'homes#top'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  #get 'lists/new'
+
+  #post 'lists' => 'lists#create'
+  #get 'lists' => 'lists#index'
+  get 'top' => 'homes#top'
+  resources :lists
+  #get 'lists/:id' => 'lists#show', as: 'list'
+  #get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  #patch 'lists/:id' => 'lists#update', as: 'update_list'
+  #delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
 end
